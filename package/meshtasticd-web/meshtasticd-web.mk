0ab913918bf4f9f69552169a8be1a49b6d6cf2bc
vidplace7 <vidplace7@gmail.com>
2024-11-10 15:40:34 -0500

    Use CPR!

--- package/meshtasticd-web/meshtasticd-web.mk
+++ package/meshtasticd-web/meshtasticd-web.mk
@@ -10,13 +10,13 @@ MESHTASTICD_WEB_SITE = https://github.com/meshtastic/web/releases/download/v$(ME
 MESHTASTICD_WEB_LICENSE = GPL-3.0
 
 define MESHTASTICD_WEB_EXTRACT_CMDS
-    $(TAR) -C $(@D) $(TAR_OPTIONS) $(MESHTASTICD_WEB_DL_DIR)/$(MESHTASTICD_WEB_SOURCE)
+	$(TAR) -C $(@D) $(TAR_OPTIONS) $(MESHTASTICD_WEB_DL_DIR)/$(MESHTASTICD_WEB_SOURCE)
 	gunzip $(@D)/*.gz
 endef
 
 define MESHTASTICD_WEB_INSTALL_TARGET_CMDS
-    mkdir -p $(TARGET_DIR)/usr/share/doc/meshtasticd/web
-	cp -a $(@D)/* $(TARGET_DIR)/usr/share/doc/meshtasticd/web
+	mkdir -p $(TARGET_DIR)/usr/share/doc/meshtasticd/web
+	cp -r $(@D)/* $(TARGET_DIR)/usr/share/doc/meshtasticd/web
 endef
 
 $(eval $(generic-package))
