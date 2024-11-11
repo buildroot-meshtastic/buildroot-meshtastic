c857937543506bcf641554b36e6a06e8543e1f35
vidplace7 <vidplace7@gmail.com>
2024-11-11 12:28:27 -0500

    Add util-linux

--- package/meshtasticd-web/meshtasticd-web.mk
+++ package/meshtasticd-web/meshtasticd-web.mk
@@ -13,8 +13,10 @@ MESHTASTICD_WEB_DEPENDENCIES = \
 	liborcania \
 	libyder \
 	libulfius \
+	ca-certificates \
 	libopenssl \
-	openssl
+	openssl \
+	util-linux
 
 define MESHTASTICD_WEB_EXTRACT_CMDS
 	$(TAR) -C $(@D) $(TAR_OPTIONS) $(MESHTASTICD_WEB_DL_DIR)/$(MESHTASTICD_WEB_SOURCE)
