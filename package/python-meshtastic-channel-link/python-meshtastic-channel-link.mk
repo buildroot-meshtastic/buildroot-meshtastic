0d1c99d376259cc92855f2d270f150425c2221d3
vidplace7 <vidplace7@gmail.com>
2024-11-15 18:21:59 -0500

    Add ChannelLink for meshtastic

---
+++ package/python-meshtastic-channel-link/python-meshtastic-channel-link.mk
@@ -0,0 +1,28 @@
+################################################################################
+#
+# python-meshtastic-channel-link
+#
+################################################################################
+
+PYTHON_MESHTASTIC_CHANNEL_LINK_VERSION = 1.0.0
+PYTHON_MESHTASTIC_CHANNEL_LINK_SITE = $(call github,pdxlocations,ChannelLink-for-Meshtastic,v$(PYTHON_MESHTASTIC_CHANNEL_LINK_VERSION))
+PYTHON_MESHTASTIC_CHANNEL_LINK_LICENSE = GPL-3.0
+PYTHON_MESHTASTIC_CHANNEL_LINK_LICENSE_FILES = LICENSE
+
+define PYTHON_MESHTASTIC_CHANNEL_LINK_INSTALL_TARGET_CMDS
+	$(INSTALL) -d -m 0755 $(TARGET_DIR)/opt/ChannelLink-for-Meshtastic
+	cp -r $(@D)/* $(TARGET_DIR)/opt/ChannelLink-for-Meshtastic
+endef
+
+# Service (Daemon) files -- Untested
+# define PYTHON_MESHTASTIC_CHANNEL_LINK_INSTALL_INIT_SYSV
+# 	$(INSTALL) -D -m 0755 $(PYTHON_MESHTASTIC_CHANNEL_LINK_PKGDIR)/S99ChannelLink \
+# 		$(TARGET_DIR)/etc/init.d/S99ChannelLink
+# endef
+
+# define PYTHON_MESHTASTIC_CHANNEL_LINK_INIT_SYSTEMD
+# 	$(INSTALL) -D -m 0644 $(PYTHON_MESHTASTIC_CHANNEL_LINK_PKGDIR)/ChannelLink.service \
+# 		$(TARGET_DIR)/usr/lib/systemd/system/ChannelLink.service
+# endef
+
+$(eval $(generic-package))
