df898c49bf313754047c3944f77ec5a735d2fd0d
vidplace7 <vidplace7@gmail.com>
2024-11-15 18:49:14 -0500

    Add base overlay

--- external.mk
+++ external.mk
@@ -1,1 +1,5 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_MESHTASTIC_PATH)/package/*/*.mk))
+
+ifeq ($(BR2_EXTERNAL_MESHTASTIC_OVERLAY),y)
+BR2_ROOTFS_OVERLAY += $(BR2_EXTERNAL_MESHTASTIC_PATH)/overlays/rootfs_overlay
+endif
