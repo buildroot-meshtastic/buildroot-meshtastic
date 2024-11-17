eacf1a8973d98c63cf78fbf5cbae350d244f5707
vidplace7 <vidplace7@gmail.com>
2024-11-17 11:21:44 -0500

    renovate platformio

--- package/python-platformio/python-platformio.mk
+++ package/python-platformio/python-platformio.mk
@@ -4,6 +4,7 @@
 #
 ################################################################################
 
+# renovate: datasource=pypi depName=python-platformio packageName=platformio versioning=semver
 PYTHON_PLATFORMIO_VERSION = 6.1.16
 PYTHON_PLATFORMIO_SOURCE = platformio-$(PYTHON_PLATFORMIO_VERSION).tar.gz
 PYTHON_PLATFORMIO_SITE = https://files.pythonhosted.org/packages/32/a0/4b1d18da2668a37b28beff3ecdc934940516302565c31a4cd4e17661a285
